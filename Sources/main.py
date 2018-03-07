624fb30ec441466f422a53f2853e0b2e13fe65c0
Thierry Girard <girard.thierry80@gmail.com>
2018-03-07 18:59:37 +0100

    Using lambda function to reduce the number of identical caculations

--- Sources/main.py
+++ Sources/main.py
@@ -12,21 +12,22 @@ Y = np.load('../data/dev_lbl.npy')
 def Apprentissage(X,Y):
     # Cas equiprobable, pas de P
     appArray = []
-    for i in range(0,9):
+    for i in range(0,10):
         app = X[Y==i]
         moy = np.mean(app,axis=0)
         Cov = np.cov(app.T)
-        appArray.append((moy,Cov))       
+        logDet = np.linalg.slogdet(Cov)
+        logDet = logDet[0] * logDet[1]
+        CovInv = np.linalg.inv(Cov)
+        appArray.append(lambda x, logDet = logDet, CovInv = CovInv: - logDet - (x - moy).T @ CovInv @ (x - moy))
     return appArray
 
-def BayesEvaluation(X,appArray):   
+def BayesEvaluation(x,appArray):   
     bestMatch = 0
-    bestScore = - np.Inf
-    for i in range(0,9):
-        moy = appArray[i][0]
-        Cov = appArray[i][1]
-        logDet = np.linalg.slogdet(Cov)
-        score = - logDet[0] * logDet[1] - (X - moy).T @ np.linalg.inv(Cov) @ (X - moy)
+    bestScore = appArray[0](x)
+    #print(bestScore)
+    for i in range(1,10):
+        score = appArray[i](x)
         #print(score)
         if(score > bestScore):
             bestMatch = i
@@ -43,14 +44,14 @@ print('START')
 tauxErreur = 0
 start_time = time()
 
-for i in range(0,X.shape[0]):
-    result = BayesEvaluation(X[i], appArray)
-    print('I =',i,'  Result = ',result ,'Expected = ',Y[i])
-    if(result != Y[i]):
+for i in range(0,X0.shape[0]):
+    result = BayesEvaluation(X0[i], appArray)
+    print('i =',i,'  Result = ', result,'Expected = ',Y0[i])
+    if(result != Y0[i]):
         tauxErreur+=1
 
 print("--- %s seconds ---" % (time() - start_time))
-print('Taux d\'erreur : ', tauxErreur/X.shape[0] )
+print('Taux d\'erreur : ', tauxErreur/X0.shape[0] )
 
 
 
