8168226b41f20a49180004fb6645f0e206a1a862
Thierry Girard <girard.thierry80@gmail.com>
2018-03-10 18:40:05 +0100

    Question 2 : done Ajout d'une boucle qui teste différente taille d'ACP, paramètré à définir

--- Sources/main.py
+++ Sources/main.py
@@ -1,16 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from time import time
+from sklearn.decomposition import PCA
 
-X0 = np.load('../data/trn_img.npy')
-Y0 = np.load('../data/trn_lbl.npy')
+Xapp = np.load('../data/trn_img.npy')
+Yapp = np.load('../data/trn_lbl.npy')
 
-X = np.load('../data/dev_img.npy')
-Y = np.load('../data/dev_lbl.npy')
+Xdev = np.load('../data/dev_img.npy')
+Ydev = np.load('../data/dev_lbl.npy')
+
+Xtest = np.load('../data/tst_img.npy')
 
 
+""" Question 1 """
+#Construction des Fonctions d'évaluation des échantillons 
 def Apprentissage(X,Y):
-    # Cas equiprobable, pas de P
     appArray = []
     for i in range(0,10):
         app = X[Y==i]
@@ -19,15 +23,17 @@ def Apprentissage(X,Y):
         logDet = np.linalg.slogdet(Cov)
         logDet = logDet[0] * logDet[1]
         CovInv = np.linalg.inv(Cov)
+        # Cas equiprobable, pas de P
         appArray.append(lambda x, logDet = logDet, CovInv = CovInv: - logDet - (x - moy).T @ CovInv @ (x - moy))
     return appArray
 
-def BayesEvaluation(x,appArray):   
+#Evaluation de l'échantillon par rapport aux fonctions dévaluation données
+def BayesEvaluation(X, appArray):   
     bestMatch = 0
-    bestScore = appArray[0](x)
+    bestScore = appArray[0](X)
     #print(bestScore)
     for i in range(1,10):
-        score = appArray[i](x)
+        score = appArray[i](X)
         #print(score)
         if(score > bestScore):
             bestMatch = i
@@ -35,26 +41,78 @@ def BayesEvaluation(x,appArray):
     return bestMatch
 
 
-appArray = Apprentissage(X0,Y0)
+""" Main :P """
 
-imgIndex = 160
-
-print('taille de l\'échantillon', X.shape[0])
-print('START')
+#Question 1
+X = Xdev
+Y = Ydev
+print("Taille de l'échantillon : ", X.shape[0])
+print("Classifieur Bayésien gaussien classique")
+appArray = Apprentissage(Xapp,Yapp)
+print("START")
 tauxErreur = 0
 start_time = time()
 
-for i in range(0,X0.shape[0]):
-    result = BayesEvaluation(X0[i], appArray)
-    print('i =',i,'  Result = ', result,'Expected = ',Y0[i])
-    if(result != Y0[i]):
+for i in range(0,X.shape[0]):
+    result = BayesEvaluation(X[i], appArray)
+    #print("i =",i,""  Result = ", result," Expected = ",Y[i])
+    if(result != Y[i]):
         tauxErreur+=1
+print("--- " ,(time() - start_time)," seconds ---")
+print("END")
+print("Taux d\'erreur : ", tauxErreur/X.shape[0],"\n")
 
-print("--- %s seconds ---" % (time() - start_time))
-print('Taux d\'erreur : ', tauxErreur/X0.shape[0] )
 
+# Question 2
+teList = []
+timeList = []
+sizeList = range(50, Xdev.shape[1], 50)
 
+#for size in range(50, Xdev.shape[1], 50):
+for size in sizeList:
+    print("Classifieur Bayésien ACP de taille ",size)
+    pca = PCA(size)
+    pca.fit(Xapp)
+    XappPCA = pca.transform(Xapp)
+    X = pca.transform(Xdev)
+    Y = Ydev
+    appArrayPCA = Apprentissage(XappPCA,Yapp)
+
+    print("START")
+    erreur = 0
+    start_time = time()
+
+    for i in range(0,X.shape[0]):
+        result = BayesEvaluation(X[i], appArrayPCA)
+        #print('i =',i,'  Result = ', result,'Expected = ',Y[i])
+        if(result != Y[i]):
+            erreur+=1
+    ti = (time() - start_time)
+    print("--- " , ti," seconds ---")
+    print("END")
+    te = (erreur/X.shape[0])
+    print("Taux d'erreur : ", te, "\n")
+    teList.append(te)
+    timeList.append(ti)
+
+fig, ax1 = plt.subplots()
+
+ax2 = ax1.twinx()
+ax1.plot(sizeList,teList,'r-o' )
+ax2.plot(sizeList,timeList,'b-x' )
+
+ax1.set_xlabel("Dimension de l'ACP")
+ax1.set_ylabel("Taux d'erreur", color='r')
+ax2.set_ylabel("Temps de traitement de l'échantillon \n(sans l'apprentissage)\nEn seconde", color='b')
+
+plt.show()
 
 """img = X[imgIndex].reshape(32,24)
 plt.imshow(img, plt.cm.gray)
+plt.show()
+
+plt.scatter(*zip(*testList2))
+plt.title('Random Figure')
+plt.xlabel('X-Axis')
+plt.ylabel('Y-Axis')
 plt.show()"""
