1c0ea99742eb877873e7761e7bd0fec0a35ddf84
Thierry Girard <girard.thierry80@gmail.com>
2018-03-15 16:17:10 +0100

    Question 3/2 + init rapport

--- Sources/main.py
+++ Sources/main.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from time import time
 from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn import preprocessing
 
 Xapp = np.load('../data/trn_img.npy')
 Yapp = np.load('../data/trn_lbl.npy')
@@ -42,35 +44,38 @@ def BayesEvaluation(X, appArray):
 
 
 """ Main :P """
-
+"""
 #Question 1
 X = Xdev
 Y = Ydev
 print("Taille de l'échantillon : ", X.shape[0])
 print("Classifieur Bayésien gaussien classique")
+start_time = time()
 appArray = Apprentissage(Xapp,Yapp)
 print("START")
-tauxErreur = 0
-start_time = time()
+erreur = 0
 
 for i in range(0,X.shape[0]):
     result = BayesEvaluation(X[i], appArray)
     #print("i =",i,""  Result = ", result," Expected = ",Y[i])
     if(result != Y[i]):
-        tauxErreur+=1
-print("--- " ,(time() - start_time)," seconds ---")
+        erreur+=1
+tiBase = (time() - start_time)
+teBase = (erreur/X.shape[0])
+print("--- " ,tiBase," seconds ---")
 print("END")
-print("Taux d\'erreur : ", tauxErreur/X.shape[0],"\n")
+print("Taux d\'erreur : ", teBase,"\n")
+
 
 
 # Question 2
 teList = []
 timeList = []
-sizeList = range(50, Xdev.shape[1], 50)
+sizeList = [i for i in range(0,50,10)]+[i for i in range(50,Xapp.shape[1],50)]
 
-#for size in range(50, Xdev.shape[1], 50):
 for size in sizeList:
     print("Classifieur Bayésien ACP de taille ",size)
+    start_time = time()
     pca = PCA(size)
     pca.fit(Xapp)
     XappPCA = pca.transform(Xapp)
@@ -80,7 +85,6 @@ for size in sizeList:
 
     print("START")
     erreur = 0
-    start_time = time()
 
     for i in range(0,X.shape[0]):
         result = BayesEvaluation(X[i], appArrayPCA)
@@ -98,21 +102,99 @@ for size in sizeList:
 fig, ax1 = plt.subplots()
 
 ax2 = ax1.twinx()
-ax1.plot(sizeList,teList,'r-o' )
-ax2.plot(sizeList,timeList,'b-x' )
+lgd1_1 = ax1.plot(sizeList,teList,'r-o',label="Taux d'erreur en fonction de la dimension de L'ACP")
+lgd1_2 = ax1.plot(Xdev.shape[1],teBase,'g-o',label="Taux d'erreur sans ACP")
+lgd2_1 = ax2.plot(sizeList,timeList,'b-x',label="Temps de traitemet en fonction de la dimension de L'ACP")
+lgd2_2 = ax2.plot(Xdev.shape[1],tiBase,'g-x',label="temps de traitement sans ACP")
+
+lgds = lgd1_1 + lgd1_2 + lgd2_1 + lgd2_2 
+
+lbls = [lgd.get_label() for lgd in lgds]
+ax1.legend(lgds, lbls, loc=4)
+
+ax1.set_xlabel("Dimension de l'ACP")
+ax1.set_ylabel("Taux d'erreur", color='r')
+ax2.set_ylabel("Temps de traitement de l'échantillon \nEn seconde", color='b')
+
+plt.show()
+
+#fig.savefig("question.png")
+
+
+"""
+#####   Question 3   #####
+start_time = time()
+#Scaling des données pour le SVM
+X = preprocessing.scale(Xdev)
+Y = Ydev
+print("Taille de l'échantillon : ", X.shape[0])
+print("Classifieur Support Vector Machines")
+
+clf = SVC(C=100000000,cache_size=4000)
+#Scaling des données pour le SVM
+clf.fit(preprocessing.scale(Xapp), Yapp) 
+
+print("START")
+erreur = 0
+result = clf.predict(X)
+tiBase = (time() - start_time)
+
+for i in range(0,X.shape[0]):
+    if(result[i] != Y[i]):
+        erreur+=1
+
+teBase = (erreur/X.shape[0])
+print("--- " ,tiBase," seconds ---")
+print("END")
+print("Taux d\'erreur : ", teBase,"\n")
+
+
+teList = []
+timeList = []
+sizeList = [i for i in range(0,50,10)]+[i for i in range(50,Xapp.shape[1],50)]
+
+for size in sizeList:
+    start_time = time()
+    #Scaling des données pour le SVM
+    pca = PCA(50)
+    pca.fit(Xapp)
+    XappPCA = pca.transform(Xapp)
+    #Scaling des données pour le SVM
+    X = preprocessing.scale(pca.transform(Xdev))
+    Y = Ydev
+    print("Taille de l'échantillon : ", X.shape[0])
+    print("Classifieur Support Vector Machines, ACP de taille ",size)
+
+    clf = SVC(C=100000000,cache_size=4000)
+    clf.fit(preprocessing.scale(XappPCA), Yapp) 
+
+    print("START")
+    erreur = 0
+    result = clf.predict(X)
+    ti = (time() - start_time)
+
+    for i in range(0,X.shape[0]):
+        if(result[i] != Y[i]):
+            erreur+=1
+
+    print("--- " , ti," seconds ---")
+    print("END")
+    te = (erreur/X.shape[0])
+    print("Taux d'erreur : ", te, "\n")
+    teList.append(te)
+    timeList.append(ti)
+
+fig, ax1 = plt.subplots()
+
+ax2 = ax1.twinx()
+ax1.plot(sizeList,teList,'r-o',label="Taux d'erreur en fonction de la dimension de L'ACP")
+ax1.plot(Xdev.shape[1],teBase,'g-o',label="Taux d'erreur sans ACP")
+ax2.plot(sizeList,timeList,'b-x',label="Temps de traitemet en fonction de la dimension de L'ACP")
+ax2.plot(Xdev.shape[1],tiBase,'g-x',label="temps de traitement sans ACP")
 
 ax1.set_xlabel("Dimension de l'ACP")
 ax1.set_ylabel("Taux d'erreur", color='r')
 ax2.set_ylabel("Temps de traitement de l'échantillon \n(sans l'apprentissage)\nEn seconde", color='b')
 
 plt.show()
-
-"""img = X[imgIndex].reshape(32,24)
-plt.imshow(img, plt.cm.gray)
-plt.show()
-
-plt.scatter(*zip(*testList2))
-plt.title('Random Figure')
-plt.xlabel('X-Axis')
-plt.ylabel('Y-Axis')
-plt.show()"""
+#fig.savefig("question3.png")
